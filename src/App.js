8dadc8a06d0cc5a1c4ac40e589e1c427cb8d9cff
David Lee <david.lee@forgerock.com>
2019-09-04 09:20:58 -0600

    change1

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload updated text.
+          Edit <code>src/App.js</code> and save to reload updated 1 text.
         </p>
         <a
           className="App-link"
